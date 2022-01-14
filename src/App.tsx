c0739fc331f2fcd28ef0af9adc6b680fa6bdc36f
Ella N <38847123+ellanan@users.noreply.github.com>
2022-01-14 16:09:21 -0500

    add attribution

--- src/App.tsx
+++ src/App.tsx
@@ -61,6 +61,9 @@ function App() {
   return (
     <div className='main-wrapper'>
       <h1 className='spacestagram-title'>Spacestagram</h1>
+      <span className='attribution'>
+        Brought to you by NASA's Astronomy Photo of the Day (APOD) API
+      </span>
       {loading ? <span>loading...</span> : null}
       <div className='card-wrapper'>
         {_.orderBy(data, ['date'], ['desc'])?.map((item) => {
