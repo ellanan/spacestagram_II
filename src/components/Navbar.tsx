f5769186f4344c53cb09e0e3a19e7c6aaf0ddc85
Ella N <38847123+ellanan@users.noreply.github.com>
2022-01-19 00:10:30 -0500

    add active styling to navlinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,10 +9,20 @@ export const Navbar = () => {
       <NavLink className='spacestagram-title' to='/'>
         Spacestagram II
       </NavLink>
-      <NavLink className='home-button' to='/'>
+      <NavLink
+        className={({ isActive }) =>
+          `navitem home-button ${isActive ? 'active' : ''}`
+        }
+        to='/'
+      >
         <AiOutlineHome size={24} />
       </NavLink>
-      <NavLink className='liked-button' to='/liked'>
+      <NavLink
+        className={({ isActive }) =>
+          `navitem liked-button ${isActive ? 'active' : ''}`
+        }
+        to='/liked'
+      >
         <AiOutlineHeart size={24} />
       </NavLink>
     </div>
